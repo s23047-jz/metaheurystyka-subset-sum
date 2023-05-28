dfba4f2423189ae3c0389af39a7eb147dcfba56e
s23047-jz <73025973+s23047-jz@users.noreply.github.com>
2023-05-28 21:39:36 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
 #include <numeric>
 
 class SubsetSum {
 private:
-    std::vector<int> list_of_numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::vector<std::vector<int>> list_of_sums = {};
-    int looking_sum_number = 0;
+    std::vector<int> listOfNumbers;
+    std::vector<std::vector<int>> listOfSums = {};
+    int targetSum = 0;
 
-    void hill_climb() {
-
-    }
-
-    void show_vector_numbers(std::vector<int> v) {
+    void showVectorNumbers(std::vector<int> v) {
         std::cout << "[ ";
         for (auto number : v) {
             std::cout << number << " ";
@@ -22,27 +16,84 @@ private:
         std::cout << "] ";
     }
 
-    void show_vectors_in_vector(std::vector<std::vector<int>> v) {
+    void showVectorsInVector(std::vector<std::vector<int>> v) {
         std::cout << "[ ";
         for (const auto& vec : v) {
-            show_vector_numbers(vec);
+            showVectorNumbers(vec);
         }
         std::cout << "]";
     }
-public:
-    SubsetSum(int num) {
-        looking_sum_number = num;
+
+    std::vector<int> generateRandomSubset() {
+        // creating a random subset
+        std::vector<int> subset;
+        for (int num : listOfNumbers) {
+            if (std::rand() % 2) {
+                subset.push_back(num);
+            }
+        }
+        return subset;
     }
 
-    void get_hill_climb() {
-        std::cout << looking_sum_number;
+    std::vector<int> generateNeighborSubset() {
+        // creating a neighbor subset
+        // W kontekście problemu „podzbiór sąsiadów” odnosi się do zmodyfikowanej wersji oryginalnego podzbioru w celu znalezienia lepszego rozwiązania.
+        // Celem generowania podzbioru sąsiadów jest zbadanie alternatywnych rozwiązań i przejście do lepszego rozwiązania w przestrzeni wyszukiwania.
+        std::vector<int> neighborSubset = listOfNumbers;
+        // dzieki module nie przekroczymy zakresu listy
+        int randomIndex = std::rand() % listOfNumbers.size();
+
+
+        if (std::find(neighborSubset.begin(), neighborSubset.end(), listOfNumbers[randomIndex]) == neighborSubset.end()) {
+            neighborSubset.push_back(listOfNumbers[randomIndex]);
+        } else {
+            neighborSubset.erase(std::remove(neighborSubset.begin(), neighborSubset.end(), listOfNumbers[randomIndex]), neighborSubset.end());
+        }
+
+        return neighborSubset;
+    }
+
+    int calculateSubsetSum(const std::vector<int>& subset) {
+        int sum = 0;
+        for (int num : subset) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    std::vector<int> hillClimb() {
+        std::vector<int> currentSet = listOfNumbers;
+        int setSum = calculateSubsetSum(listOfNumbers);
+
+        while (setSum != targetSum) {
+            std::vector<int> neighborSubset = generateNeighborSubset();
+            int neighborSum = calculateSubsetSum(neighborSubset);
+
+            if (abs(neighborSum - targetSum) < abs(setSum - targetSum)) {
+                currentSet = neighborSubset;
+                setSum = neighborSum;
+            }
+        }
+        showVectorNumbers(currentSet);
+        return currentSet;
+    }
+
+public:
+    SubsetSum(const std::vector<int>& numberSet, int num) {
+        listOfNumbers = numberSet;
+        targetSum = num;
+    }
+
+    void getHillClimb() {
+        showVectorNumbers(hillClimb());
     }
 
 };
 
 int main() {
-    SubsetSum subsetSum(6);
-    subsetSum.get_hill_climb();
+    std::vector<int> list_of_numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    SubsetSum subsetSum(list_of_numbers, 8);
+    subsetSum.getHillClimb();
 
     return 0;
 }
